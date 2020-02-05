57c213b0146ffc57e72ed99419d742ece515a3da
Michael Dobrzynski <michaeldobrzynski2@gmail.com>
2020-02-04 21:02:49 -0700

    adding barebones for the API

---
+++ Midland/Midland/Controllers/ACIManagmentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Midland.Models;
+
+namespace Midland.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ACIManagmentController : ControllerBase
+    {
+
+        private readonly ILogger<ACIManagmentController> _logger;
+
+        private List<ACIGroup> AciGroups;
+
+        public ACIManagmentController(ILogger<ACIManagmentController> logger)
+        {
+            _logger = logger;
+
+            this.AciGroups = new List<ACIGroup>();
+        }
+
+        [HttpGet]
+        public IEnumerable<ACIGroup> Get()
+        {
+            return this.AciGroups;
+        }
+    }
+}
